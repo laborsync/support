c0e4546841d9faafafe98444b91f64f904772cdf
Brian Pinney <bpinney@laborsync.com>
2015-07-29 11:15:48 -0400

    Enable intercom button for all visitors

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ var config = {
 	content_dir: './content/',
 
 	// Add your analytics tracking code (including script tags)
-	analytics: "<script>window.intercomSettings = { app_id: 'qz8lpmbr' };</script><script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qz8lpmbr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>"
+	analytics: "<script>window.intercomSettings = { app_id: 'qz8lpmbr', widget: {activator: '#IntercomDefaultWidget'} };</script><script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qz8lpmbr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>"
 
 };
 
